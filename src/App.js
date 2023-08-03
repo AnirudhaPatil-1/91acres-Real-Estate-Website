c473ca886e0bc11488e21678c54b4d2b9aa4f029
AnirudhaPatil-1 <aninp2020@gmail.com>
2023-08-04 00:37:46 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +17,8 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+      <p>Test</p>
     </div>
   );
 }
