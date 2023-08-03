21afcd9ae09697f23dc46878f19e0a8746ae3e4d
AnirudhaPatil-1 <aninp2020@gmail.com>
2023-08-04 03:41:49 +0530

    updated - route created, header component added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 // import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import {Routes, Route} from "react-router-dom";
+
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+import Home from "./pages/Home";
+import Banner from "./components/Banner";
 
 function App() {
   return (
-    <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
-      <p className="text-lg"> testTest</p>
+    <div className="max-w-[1440px] mx-auto bg-white ">
+       <Header/>
+       <Banner/>
+       <Routes>
+        <Route path="/" element={<Home/>} />
+       </Routes>
+       <Footer/>
     </div>
   );
 }
