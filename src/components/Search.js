244f51c9e76fede4acba012fbc8bf0de94c976a8
AnirudhaPatil-1 <aninp2020@gmail.com>
2023-08-04 20:01:27 +0530

    add element - add mock search element

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,8 +1,29 @@
-import React from "react";
+import React, { useContext } from "react";
+import CountryDropdown from "./CountryDropdown";
+import DateDropdown from "./DateDropdown";
+import PriceRangeDropdown from "./PriceRangeDropdown";
+import PropertyDropdown from "./PropertyDropdown";
+
+import {RiSearch2Line} from 'react-icons/ri';
+
+import {HouseContext} from './HouseContext' 
+
 
 const Search = () => {
+    // const{houses} = useContext(HouseContext);
+    const {handleClick} = useContext(HouseContext);
     return(
-        <div>Search</div>
+        <div className="px-[30px] py-6 max-w-[1400px] mx-auto flex flex-col lg:flex-row justify-between gap-4 lg:gap-x-3 relative  lg:bg-white rounded-lg">
+            <CountryDropdown/>
+            <DateDropdown/>
+            <PriceRangeDropdown/>
+            <PropertyDropdown/>
+            <button 
+            onClick={() => {handleClick()}}
+            className="bg-violet-700 hover:bg-violet-800 transition w-full lg:max-w-[162px] h-16 rounded-lg flex justify-center items-center text-white">
+                <RiSearch2Line/>
+            </button>
+        </div>
     )
 }
 
