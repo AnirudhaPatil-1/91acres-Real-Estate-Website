1428f1d7e832d25cf47fd7613f3ff5d5a96d37be
AnirudhaPatil-1 <aninp2020@gmail.com>
2023-08-04 20:16:36 +0530

    new element - parent element for banned and housing cards created and styled

---
+++ src/components/DateDropdown.js
@@ -0,0 +1,139 @@
+import React, {useState, useEffect, useContext} from 'react';
+
+import {RiCalendarLine, RiArrowDownSLine, RiArrowUpSLine} from 'react-icons/ri';
+
+import {Menu} from '@headlessui/react';
+
+import {HouseContext} from './HouseContext';
+
+const DateDropdown = () => {
+  const {date, setDate}  = useContext(HouseContext);
+  const [isOpen, setIsOpen] = useState(false);
+
+  const dates = [
+    {
+      value: 'Move-in Date (any)',
+    },
+    {
+      value: '1',
+    },
+    {
+      value: '2',
+    },
+    {
+      value: '3',
+    },
+    {
+      value: '4',
+    },
+    {
+      value: '5',
+    },
+    {
+      value: '6',
+    },
+    {
+      value: '7',
+    },
+    {
+      value: '8',
+    },
+    {
+      value: '9',
+    },
+    {
+      value: '10',
+    },
+    {
+      value: '11',
+    },
+    {
+      value: '12',
+    },
+    {
+      value: '13',
+    },
+    {
+      value: '14',
+    },
+    {
+      value: '15',
+    },
+    {
+      value: '16',
+    },
+    {
+      value: '17',
+    },
+    {
+      value: '18',
+    },
+    {
+      value: '19',
+    },
+    {
+      value: '20',
+    },
+    {
+      value: '21',
+    },
+    {
+      value: '22',
+    },
+    {
+      value: '23',
+    },
+    {
+      value: '24',
+    },
+    {
+      value: '25',
+    },
+    {
+      value: '26',
+    },
+    {
+      value: '27',
+    },
+    {
+      value: '28',
+    },
+    {
+      value: '29',
+    },
+    {
+      value: '30',
+    },
+    
+  ]
+
+  return (
+    <Menu as='div' className='dropdown relative'>
+      <Menu.Button onClick={() => {setIsOpen(!isOpen)}} className="dropdown-btn w-full text-left  hover:bg-violet-400 hover:rounded-lg hover:text-white ">
+        <RiCalendarLine className=' dropdown-icon-primary' />
+        <div >
+          <div className='text-[15px] font-medium leading-tight'>{date}</div>
+          <div className='text-[13px]'>Choose date</div>       
+        </div>
+        { isOpen? (
+              <RiArrowUpSLine className='dropdown-icon-secondary'/>
+            ):(
+              <RiArrowDownSLine className="dropdown-icon-secondary" />
+            )
+          }
+      </Menu.Button>
+      <Menu.Items className='dropdown-menu h-[400px] overflow-scroll overflow-x-hidden'>
+        {dates.map((date, index) => {
+          return(
+            <Menu.Item onClick={() => {setDate(date.value)}} className="cursor-pointer hover:text-violet-700 transition" as='li' key={index}>
+              {date.value }
+            </Menu.Item>
+          )
+        })}
+      </Menu.Items>
+    </Menu>
+    // <div>CountryDropdown</div>
+  )
+}
+
+export default DateDropdown;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,14 @@
 import React from "react";
 
+import Banner from "../components/Banner"; 
+import HouseList from "../components/HouseList";
+
 const Home = () => {
     return(
-        <div>Home</div>
+        <div className="min-h-[1800px] ">
+            <Banner/> 
+            <HouseList/>
+        </div>
     )
 }
 
