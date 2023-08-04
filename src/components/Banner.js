aaadf047baa2132f868ab301511605aa09782956
AnirudhaPatil-1 <aninp2020@gmail.com>
2023-08-04 20:06:06 +0530

    create element - consisting of search element

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -4,13 +4,14 @@ import Search from "./Search";
 
 const Banner = () => {
     return (
-        <div>
+        <section className="h-full max-h-[640px] mb-8 xl:mb-24">
             <div className="flex justify-around items-center ">
                 <h1 className="text-3xl font-bold text-gray-800">Search properties to rent</h1>
-                <div className="border-2 px-4 py-2 rounded-lg text-gray-500 font-medium hover:cursor-pointer">Search with Search Bar</div>
+                <div className="border-2 px-4 py-2 rounded-lg text-gray-500 font-medium  hover:bg-violet-300 hover:text-white hover:cursor-text">Search with Search Bar</div>
             </div>
             <Search/>
-        </div>
+        </section>
+        
         
     )
 }
