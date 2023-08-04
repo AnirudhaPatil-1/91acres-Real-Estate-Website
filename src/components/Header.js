3c695a64d33031d88d4c2a20711f820528a35da4
AnirudhaPatil-1 <aninp2020@gmail.com>
2023-08-04 20:00:51 +0530

    styling - styling header components for screen size

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,20 +7,20 @@ import {Link} from "react-router-dom";
 const Header = () => {
     return (
         <header className="py-6 mb-12 border-b">
-            <div className="container mx-auto flex justify-between items-center" >
+            <div className="container mx-auto flex justify-between items-center font-bold text-gray-800" >
                 <div className="flex justify-between items-center gap-6">
                     <Link to="/">
                         {/* <img src={Logo} alt="logo"/> */}
                         <p className="text-violet-700 hover:text-violet-800 text-3xl font-bold transition">RealState</p>
                     </Link>
-                    <Link className="px-4 py-3 bg-violet-300 rounded-lg" to="">Rent</Link>
-                    <Link to="">Buy</Link>
-                    <Link to="">Sell</Link>
-                    <Link to="">Manage Property</Link>
-                    <Link to="">Resources</Link>
+                    <Link className="px-4 py-3 bg-violet-300 text-white rounded-lg" to="">Rent</Link>
+                    <Link className="px-4 py-3 hover:bg-violet-300 hover:text-white rounded-lg" to="">Buy</Link>
+                    <Link className="px-4 py-3 hover:bg-violet-300 hover:text-white rounded-lg"  to="">Sell</Link>
+                    <Link className="px-4 py-3 hover:bg-violet-300 hover:text-white rounded-lg"  to="">Manage Property</Link>
+                    <Link className="px-4 py-3 hover:bg-violet-300 hover:text-white rounded-lg"  to="">Resources</Link>
                 </div>
                 <div className="flex items-center gap-6 ">
-                    <Link className="border-2 text-violet-800  px-4 py-3 rounded-lg hover:bg-violet-800 hover:text-white transition" to="">Log in</Link>
+                    <Link className="border-2 text-violet-800  px-4 py-3 rounded-lg hover:bg-violet-300 hover:text-white transition" to="">Log in</Link>
                     <Link className="bg-violet-700 hover:bg-violet-800 text-white px-4 py-3 rounded-lg transition " to="">Sign up</Link>
                 </div>
 
