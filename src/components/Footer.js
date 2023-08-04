c424f081fd6374692fc0832961e5523f2027f20b
AnirudhaPatil-1 <aninp2020@gmail.com>
2023-08-04 20:23:41 +0530

    element added - footer - creator tag

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,16 @@
 import React from "react";
+import { RiHeart2Fill} from "react-icons/ri";
 
 const Footer = () => {
     return (
-        <div>Footer</div>
+        <div className="bg-violet-700 w-full h-15 text-white">
+            <div className="flex justify-center items-center p-5 gap-4 text-xl" >
+                <p>Made with </p>
+                <RiHeart2Fill className="text-red-500 text-3xl"/>
+                <p> by <span className="text-red-500">Anirudha</span> </p>
+                
+            </div>
+        </div>
     )
 }
 
