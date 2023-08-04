1752604e889548d05ff39da9eccba1702f501a25
AnirudhaPatil-1 <aninp2020@gmail.com>
2023-08-04 20:13:42 +0530

    feature - filter functionality - property type element added and styled

---
+++ src/components/PropertyDropdown.js
@@ -0,0 +1,43 @@
+import React, {useState, useEffect, useContext} from 'react';
+
+import {RiHome5Line, RiArrowDownSLine, RiArrowUpSLine} from 'react-icons/ri';
+
+import {Menu} from '@headlessui/react';
+
+import {HouseContext} from './HouseContext';
+
+const PropertyDropdown = () => {
+  const {property, setProperty, properties}  = useContext(HouseContext);
+   
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <Menu as='div' className='dropdown relative'>
+      <Menu.Button onClick={() => {setIsOpen(!isOpen)}} className="dropdown-btn w-full text-left  hover:bg-violet-400 hover:rounded-lg hover:text-white">
+        <RiHome5Line className=' dropdown-icon-primary' />
+        <div >
+          <div className='text-[15px] font-medium leading-tight'>{property}</div>
+          <div className='text-[13px]'>Select property type</div>          
+        </div>
+          { isOpen? (
+              <RiArrowUpSLine className='dropdown-icon-secondary'/>
+            ):(
+              <RiArrowDownSLine className="dropdown-icon-secondary" />
+            )
+          }
+      </Menu.Button>
+      <Menu.Items className='dropdown-menu'>
+        {properties.map((property, index) => {
+          return(
+            <Menu.Item onClick={() => {setProperty(property)}} className="cursor-pointer hover:text-violet-700 transition" as='li' key={index}>
+              {property}
+            </Menu.Item>
+          )
+        })}
+      </Menu.Items>
+    </Menu>
+    // <div>PropertyDropdown</div>
+  )
+}
+
+export default PropertyDropdown;
