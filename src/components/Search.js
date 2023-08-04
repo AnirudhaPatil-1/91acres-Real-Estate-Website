b07813a970f091cfbe5f58e058fb5e21f2283769
AnirudhaPatil-1 <aninp2020@gmail.com>
2023-08-04 20:45:54 +0530

    styling - minor changes - components sizing and alignment

--- src/components/Search.js
+++ src/components/Search.js
@@ -13,14 +13,14 @@ const Search = () => {
     // const{houses} = useContext(HouseContext);
     const {handleClick} = useContext(HouseContext);
     return(
-        <div className="px-[30px] py-6 max-w-[1400px] mx-auto flex flex-col lg:flex-row justify-between gap-4 lg:gap-x-3 relative  lg:bg-white rounded-lg">
+        <div className="px-[20px] py-6 max-w-[1200px] mx-auto flex flex-col lg:flex-row justify-between gap-3 lg:gap-x-3 relative  lg:bg-white rounded-lg">
             <CountryDropdown/>
             <DateDropdown/>
             <PriceRangeDropdown/>
-            <PropertyDropdown/>
+            <PropertyDropdown />
             <button 
             onClick={() => {handleClick()}}
-            className="bg-violet-700 hover:bg-violet-800 transition w-full lg:max-w-[162px] h-16 rounded-lg flex justify-center items-center text-white">
+            className="bg-violet-600 hover:bg-violet-700 transition w-full lg:max-w-[150px] h-16 rounded-lg flex justify-center items-center text-white text-xl">
                 <RiSearch2Line/>
             </button>
         </div>
